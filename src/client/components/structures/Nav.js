80b089f37dd047c55c29e2ae7496dc076f0e4e94
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-07-20 22:21:33 +0200

    add images file and modify footer style

--- src/client/components/structures/Nav.js
+++ src/client/components/structures/Nav.js
@@ -1,51 +1,34 @@
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import React from "react";
- import Nav from "react-bootstrap/Nav";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 const NavBar = () => {
   return (
-    // <>
-    //   {/* <Nav variant="tabs" defaultActiveKey="/home">
-    //     <Nav.Item>
-    //       <Nav.Link>
-    //         <Link to="/"> Home</Link>
-    //       </Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link eventKey="link-1">
-    //         <Link to="/meals"> Meals</Link>
-    //       </Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link>
-    //         <Link to="/about"> About</Link>
-    //       </Nav.Link>
-    //     </Nav.Item>
-    //   </Nav> */}
-    // </>
     <div>
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link ><Link to="/"> Home</Link></Nav.Link>
-        <Nav.Link ><Link to="/meals"> Meals</Link></Nav.Link>
-        <Nav.Link ><Link to="/about"> About</Link></Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
-    </Navbar>
-    
-  
-  
-  </div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link>
+            <Link to="/"> Home</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/meals"> Meals</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/about"> About</Link>
+          </Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+    </div>
   );
 };
 
--- src/client/components/structures/Title.js
+++ src/client/components/structures/Title.js
@@ -1,14 +1,15 @@
 import React from "react";
-import './main.css'
-import {image} from 'bg.png'
-const Title =()=>{
-
-    return(
-        <div>
-        <h1>Wellcom to Meal Sharing website </h1>
-        <img src={image}/>
-
-        </div>
-    )
-}
-export default Title
+import "./main.css";
+// import image from './bg.png'
+const Title = () => {
+  return (
+    <div>
+      <h1>Welcome to Meal Sharing website </h1>
+      <img
+        src="https://p0.piqsels.com/preview/13/60/32/vietnam-ho-chi-minh-city-noodle-foodphotography.jpg   "
+        width="1100px" height="500px"
+      />
+    </div>
+  );
+};
+export default Title;
