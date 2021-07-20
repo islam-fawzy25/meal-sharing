462d61cb2eabf8ba8556e4b0dd1b7d4d3459b464
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-07-20 19:30:27 +0200

    test

--- src/client/components/structures/Title.js
+++ src/client/components/structures/Title.js
@@ -1,9 +1,14 @@
 import React from "react";
 import './main.css'
+import {image} from 'bg.png'
 const Title =()=>{
 
     return(
+        <div>
         <h1>Wellcom to Meal Sharing website </h1>
+        <img src={image}/>
+
+        </div>
     )
 }
 export default Title
