d2be06dda21ce390d4f3f496faaad230cf0e50fd
Islam <islam.fawzy25@hotmail.com>
2022-01-15 15:51:15 +0100

    button bootstrap added

--- src/client/components/reservations/ReservationForm.js
+++ src/client/components/reservations/ReservationForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./reservations.css";
 import { fetchFromDb } from "../../helper/fetch/fetch";
 import { useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const ReservationForm = () => {
   const [phone, setPhone] = useState();
@@ -14,7 +15,7 @@ const ReservationForm = () => {
   const mealId = Number(param.id)
 
   const newReservation = async (e) => {
-    try { 
+    try {
       await fetchFromDb("/reservations", "post", { phone, name, email, mealId, date, guestsNumber })
       return e.preventDefault();
     } catch (error) {
@@ -24,56 +25,60 @@ const ReservationForm = () => {
 
   return (
     <>
-        <div className={`reservation-form-container`} >
-          <div className="reservation-form" >
-            <form onSubmit={() => {
-              newReservation()
-            }}>
-              <input
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Enter your name"
-                type="text"
-                minLength="2"
-                required
-              />
-              <hr />
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter email"
-                type="text"
-                minLength="8"
-                required
-              />
-              <hr />
-              <input
-                onChange={(e) => setPhone(e.target.value)}
-                placeholder="Enter your number"
-                type="text"
-                minLength="6"
-                required
-              />
-              <hr />
-              <br />
-              <input
-                onChange={(e) => setDate(e.target.value)}
-                type="date"
-                required
-                pattern="\d{4}-\d{2}-\d{2}"
-              />
-              <hr />
-              <input
-                onChange={(e) => setGuestsNumber(e.target.value)}
-                placeholder="Number of reservation"
-                type="number"
-                required
-              />
-              <hr />
-              <button > Book your seat</button>
-            </form >
-          </div>
+      <div className={`reservation-form-container`} >
+        <div className="reservation-form" >
+          <form onSubmit={() => {
+            newReservation()
+          }}>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter your name"
+              type="text"
+              minLength="2"
+              required
+            />
+            <hr />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+              type="text"
+              minLength="8"
+              required
+            />
+            <hr />
+            <input
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder="Enter your number"
+              type="text"
+              minLength="6"
+              required
+            />
+            <hr />
+            <br />
+            <input
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+              required
+              pattern="\d{4}-\d{2}-\d{2}"
+            />
+            <hr />
+            <input
+              onChange={(e) => setGuestsNumber(e.target.value)}
+              placeholder="Number of reservation"
+              type="number"
+              required
+            />
+            <hr />
+            <div className="d-grid gap-2">
+              <Button variant="secondary" size="lg">
+                Book your seat
+              </Button>
+            </div>
+          </form >
         </div>
-      
-       {/* need functionality for this parts */}
+      </div>
+
+      {/* need functionality for this parts */}
       {/* {!available && <h1 className='no-reservation-message'> no available reservation </h1>
       }
       {!isReserv && <div className="reservation-message">
--- src/client/containers/SingleMealPage/SingleMeal.container.js
+++ src/client/containers/SingleMealPage/SingleMeal.container.js
@@ -34,7 +34,6 @@ export default function SingleMealPage() {
         <>
             <MealById mealById={mealById} />
             <ReservationForm />
-
         </>
     )
 }
